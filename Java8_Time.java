1d2af691315f1e057cbc36ca04efb2488f482bef
Shreyas MN <shreyaschinni@gmail.com>
2016-09-07 22:37:16 -0400

    Add files via upload

---
+++ Java8_Time.java
@@ -0,0 +1,33 @@
+package java_8;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+/**
+ * @author ${Shreyas Mahanthappa Nagaraj}
+ * 
+ */
+public class Java8_Time {
+
+	public static void main(String[] args) {
+			
+		LocalDateTime datetime = LocalDateTime.now();
+		System.out.println("\nLocal Date and Time : "+datetime);
+		
+		LocalDate date = LocalDate.of(2016, Month.SEPTEMBER, 8);
+		System.out.println("\nToday's Date : "+date);
+		
+		LocalTime time = LocalTime.now();
+		System.out.println("\nLocal Time right Now : "+time);
+		
+		LocalDateTime minDateTime = LocalDateTime.MIN;
+		System.out.println("\nTime starts from : "+minDateTime);
+		
+		LocalDateTime maxDateTime = LocalDateTime.MAX;
+		System.out.println("\nMax Date Time Possible : "+maxDateTime);
+		
+	}
+
+}
