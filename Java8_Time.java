646d76ff2ec52a7427f4ccfa47dc656c059c99e4
Shreyas MN <shreyaschinni@gmail.com>
2016-09-07 22:46:45 -0400

    Update Java8_Time.java

--- Java8_Time.java
+++ Java8_Time.java
@@ -1,5 +1,6 @@
 package java_8;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,13 +8,20 @@ import java.time.Month;
 
 /**
  * @author ${Shreyas Mahanthappa Nagaraj}
- * Illustartes the Date and Time Operations in Java 8
  * 
  */
 public class Java8_Time {
 
 	public static void main(String[] args) {
 			
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		
+		Month month = currentDateTime.toLocalDate().getMonth();
+		DayOfWeek week = currentDateTime.toLocalDate().getDayOfWeek();
+		int day = currentDateTime.toLocalDate().getDayOfMonth();
+		int secs = currentDateTime.toLocalTime().getSecond();
+		System.out.println("Month : "+month + ", Day of week : "+week + ", Day : "+day+ ", seconds : "+secs);
+		
 		LocalDateTime datetime = LocalDateTime.now();
 		System.out.println("\nLocal Date and Time : "+datetime);
 		
